436e946f0a68dd410309654d81ee3c6fb1a2ca3d
Roman <rabalan83@gmail.com>
2023-07-31 23:05:45 +0300

    Разделение кода по смысловым направлениям

---
+++ kivy_reserved_words/classes/Button_If.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+KV = """
+BoxLayout:
+	orientation: 'vertical'
+	Button:
+		id: bt1
+		text: "Кнопка 1"
+	Label:
+		text: "Отпущена" if bt1.state == "normal" else "Нажата"
+"""
+
+class MainApp(App):
+	def build(self):
+		return Builder.load_string(KV)
+
+def run_button_if():
+	MainApp().run()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import kivy_first_app as k
+import kivy_first_apps as k
+import kivy_reserved_words as kk
 
-k.run_button_self()
+# k.run_k_my_class4()
+kk.run_simpl_calc()
