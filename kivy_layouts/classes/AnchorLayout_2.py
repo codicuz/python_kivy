dfeca192f2476e9141f1a7ee543004c2739011a9
Roman <rabalan83@gmail.com>
2023-08-08 18:35:11 +0300

    AnchorLayout

---
+++ kivy_layouts/classes/AnchorLayout_2.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+KV = '''
+BoxLayout:
+	AnchorLayout:
+		anchor_x: 'left'
+		anchor_y: 'top'
+		padding: [100, 100, 100, 100]
+		# padding: [10, 100]
+		# padding: [40]
+		Button:
+			text: 'Кнопка'
+'''
+
+class MainApp(App):
+	def build (self):
+		return Builder.load_string(KV)
+
+MainApp().run()
