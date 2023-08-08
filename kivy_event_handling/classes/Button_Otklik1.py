a1202c8e0105161931dafa7d83c6b47395c61bf5
Roman <rabalan83@gmail.com>
2023-08-08 12:12:19 +0300

    Цвет фона виджетов и обработка события. Явное связывание с функцией

---
+++ kivy_event_handling/classes/Button_Otklik1.py
@@ -0,0 +1,21 @@
+# Явное связывание визуального элемента с заданной функцией
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MainApp(App):
+	def build(self):
+		button = Button(
+			text='Кнопка',
+			size_hint = (.5, .5),
+			pos_hint= {'center_x': .5, 'center_y': .5}
+		)
+		
+		button.bind(on_press=self.press_button)
+		
+		return button
+
+	def press_button (self, instance):
+		print('Вы нажали на кнопку!')
+		
+MainApp().run()
