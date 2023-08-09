1d4af552d0150b992603199dede8a2d97dc3cecf
Roman <rabalan83@gmail.com>
2023-08-09 10:30:14 +0300

    BoxLayout

---
+++ kivy_layouts/classes/BoxLayout_1.py
@@ -0,0 +1,20 @@
+# виждет BoxLayout имеет следующие свойства:
+# – orientation: – ориентация, или порядок расположения дочерних элементов (’vertical’ – вертикальное, ’horizontal’ – горизонтальное);
+# – padding (отступ) – задает величину отступа виджета от границ его родительского контейнера (в пикселах).
+# – spacing (интервал) – расстояние между дочерними элементами, находящимися внутри контейнера (в пикселях).
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class MyApp (App):
+	def build (self):
+		layout = BoxLayout(orientation='vertical', spacing=10)
+		#layout = BoxLayout(orientation='horizontal')
+		btn1 = Button (text='Кнопка 1')
+		btn2 = Button (text='Кнопка 2')
+		layout.add_widget(btn1)
+		layout.add_widget(btn2)
+		return layout
+
+MyApp().run()
