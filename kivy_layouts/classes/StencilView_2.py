b67363f6fe6d9f2be96c25ae673c59bb6743c631
Roman <rabalan83@gmail.com>
2023-08-11 12:57:40 +0300

    StencilView

---
+++ kivy_layouts/classes/StencilView_2.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.lang import Builder
+KV = '''
+StencilView:
+    Scatter:
+        Image:
+            source: '../../resources/background.jpg'
+'''
+
+class MainApp(App):
+    def build(self):
+        return Builder.load_string(KV)
+
+MainApp().run()
