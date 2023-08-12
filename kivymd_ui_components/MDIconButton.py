ce9865c9831595c1859b246f30d6e282e2e949dd
Roman <rabalan83@gmail.com>
2023-08-12 22:56:19 +0300

    Кнопки KivyMD

--- kivymd_ui_components/MDIconButton.py
+++ kivymd_ui_components/MDIconButton.py
@@ -4,10 +4,10 @@ from kivy.lang import Builder
 KV = '''
 MDScreen:
 	MDIconButton:
-		#icon: 'language-python'
-		icon: '../resources/images/main_icon.png'
+		icon: 'language-python'
+		# icon: '../resources/images/main_icon.png'
 		user_font_size: '64sp'
-		theme_text_color: 'Custom'
+		# theme_text_color: 'Custom'
 		text_color: app.theme_cls.primary_color
 		pos_hint: {'center_x': .5, 'center_y': .5}
 		on_press: print('Кнопка нажата')
@@ -16,7 +16,8 @@ MDScreen:
 
 
 class MainApp(MDApp):
- 	def build(self):
- 		return Builder.load_string(KV)
+    def build(self):
+        return Builder.load_string(KV)
+
 
 MainApp().run()
